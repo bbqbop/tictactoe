7b6885807d2da4d078eb34b8ef78176b042176bd
bbqbop <dierk.peters@gmail.com>
2023-04-07 10:26:03 -0400

    add win & draw logic

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const gameBoard = (function() {
+const game = (function() {
     const gameBoard = []
-    const makeMove = function(player, pos) {
-        gameBoard[pos - 1] = player;
+    const makeMove = function(curPlayer, pos) {
+        if(gameBoard[pos -1]) {
+            player.currentPlayer()
+            return;
+        }
+        gameBoard[pos - 1] = curPlayer;
         displayController.update(gameBoard)
+        isGameOver()
     }
+    const isGameOver = function(){
+        const threes = [[gameBoard[0], gameBoard[1], gameBoard[2]],
+                        [gameBoard[3], gameBoard[4], gameBoard[5]],
+                        [gameBoard[6], gameBoard[7], gameBoard[8]],
+                        [gameBoard[0], gameBoard[3], gameBoard[6]],
+                        [gameBoard[1], gameBoard[4], gameBoard[7]],
+                        [gameBoard[2], gameBoard[5], gameBoard[8]],
+                        [gameBoard[0], gameBoard[4], gameBoard[8]],
+                        [gameBoard[2], gameBoard[4], gameBoard[6]],
+                        ];
+        // Check for win  
+        threes.forEach(row => {
+            if (row[0] != undefined && row[0] === row[1] && row[1] === row[2]){
+                console.log(`${row[0]} Wins`)
+            }
+        });
+        // Check for draw
+        if(threes.every(row => {
+            return row[0] != row[1] && row[0] != undefined && row[1] != undefined || 
+                   row[0] != row[2] && row[0] != undefined && row[2] != undefined || 
+                   row[1] != row[2] && row[1] != undefined && row[2] != undefined
+        })) {
+            console.log('DRAW')
+        };
+    };
     return {
         makeMove
     }
@@ -22,25 +52,29 @@ const player = (function(){
 const displayController = (function() {
     const board = document.querySelector('.gameboard');
     const drawFields = (function(){
-        for(let i = 1; i <= 9; i++){
+        for (let i = 1; i <= 9; i++) {
             const field = document.createElement('div');
             field.classList.add(`field${i}`);
             field.addEventListener('click', (e) => {
                 const getPlayer = player.currentPlayer()
                 const pos = parseInt(e.target.className[5])
-                gameBoard.makeMove(getPlayer, pos)
+                game.makeMove(getPlayer, pos)
             })
             board.append(field);
         }
     })()
     const update = function(gameBoard) {
-        console.log('asjaks')
         const fields = document.querySelectorAll('div[class^="field');
         fields.forEach(field => {
-            field.textContent = 'haha'
+            const pos = parseInt(field.className[5])
+            field.textContent = gameBoard[pos - 1]
         })
     }
     return {
         update
     }
 })();
+
+
+
+
